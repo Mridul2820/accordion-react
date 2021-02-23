4cb0c7b4507d53cf3ec0091f80337571b5dff0e3
Mridul2820 <immridulpanda2820@gmail.com>
2021-02-23 12:34:35 +0530

    accordians created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { useState } from 'react'
 import Question from './components/Question'
+import data from './data'
 import './App.scss';
 
 import React from 'react'
 
 const App = () => {
+    const [questions, setQuestions] = useState(data)
+
     return (
-        <div>
-            <h1>hey</h1>
-        </div>
+        <main>
+            <div className="container">
+                <h3>Question Answers about Login</h3>
+                <section className="info">
+                    {questions.map(question => {
+                        return <Question key={question.id} {...question} />
+                    })}
+                </section>
+            </div>
+        </main>
     )
 }
 
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import { useState } from 'react'
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 import './Question.scss'
 
-const Question = () => {
+const Question = ({ title, info }) => {
+    const [showInfo, setShowInfo] = useState(false)
+
     return (
-        <div>
+        <article className="question">
+            <header>
+                <h4>{title}</h4>
+                <button 
+                    className="btn" 
+                    onClick={() => setShowInfo(!showInfo)}
+                >
+                    {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus /> }
+                </button>
+            </header>
             
-        </div>
+            {showInfo && <p>{info}</p>}
+        </article>
     )
 }
 
